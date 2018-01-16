3498268304863cf401e3cb69fb428375abbdd82d
operatorequals <john.torakis@gmail.com>
2018-01-16 10:17:09 +0400

    Bumping to 0.5.12 to properly add PyPI docs

--- httpimport.py
+++ httpimport.py
@@ -25,7 +25,7 @@ except:
     from urllib.request import urlopen
 
 __author__ = 'John Torakis - operatorequals'
-__version__ = '0.5.11'
+__version__ = '0.5.12'
 __github__ = 'https://github.com/operatorequals/httpimport'
 
 log_FORMAT = "%(message)s"
