f8fef1623326ec03809a414f900b1214ec4c3518
operatorequals <john.torakis@gmail.com>
2018-01-16 08:58:35 +0400

    Bumped to '0.5.11' to add 'PyPI long description

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 from setuptools import setup
 
+
 import httpimport
+
+
+# Get the long description from the README file
+# And also convert to reST
+# MD to RST Convert Line acquired from:
+# https://bons.ai/blog/markdown-for-pypi ->
+# https://github.com/BonsaiAI/bonsai-config/blob/0.3.1/setup.py#L9
+try:
+	from pypandoc import convert
+
+	def read_md(f): return convert(f, 'rst')
+
+except ImportError:
+	print("warning: pypandoc module not found, could not convert Markdown to RST")
+	def read_md(f): return open(f, 'r').read()
+
 
 setup(name='httpimport',
 	  version=httpimport.__version__,
 	  description='Module for remote in-memory Python package/module loading through HTTP',
+	  long_description=read_md('README.md')
 	  author=httpimport.__author__,
 	  author_email='john.torakis@gmail.com',
 	  license='Apache2',
