b57c7b31df4080dd15fcc16c8aac8cda084dbd71
rkbennett <r.k.bennett@hotmail.com>
2022-12-28 10:18:38 -0600

    removes replace exclusion on final '.' in _mod_to_filename, also switches os.sep to '/' for url

--- httpimport.py
+++ httpimport.py
@@ -117,17 +117,19 @@ It is better to not use this class directly, but through its wrappers ('remote_r
 
     def _mod_to_filepaths(self, fullname, compiled=False):
         suffix = '.pyc' if compiled else '.py'
-        # get the python module name
-        py_filename = fullname.replace(".", os.sep) + suffix
-        # get the filename if it is a package/subpackage
-        py_package = fullname.replace(
-            ".", os.sep, fullname.count(".") - 1) + "/__init__" + suffix
-
         if self.is_archive:
+            # get the python module name
+            py_filename = fullname.replace(".", os.sep) + suffix
+            # get the filename if it is a package/subpackage
+            py_package = fullname.replace(
+                ".", os.sep, fullname.count(".") - 1) + "/__init__" + suffix
             return {'module': py_filename, 'package': py_package}
         else:
             # if self.in_progress:
-            # py_package = fullname.replace(".", '/') + "/__init__" + suffix
+            # get the python module name
+            py_filename = fullname.replace(".", '/') + suffix
+            # get the filename if it is a package/subpackage
+            py_package = fullname.replace(".", '/') + "/__init__" + suffix
             return {
                 'module': self.base_url + py_filename,
                 'package': self.base_url + py_package
